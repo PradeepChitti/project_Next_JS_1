9a42b8779156bab632b06a8c9252e4f6c96b770e
PradeepChitti <chitti.pradeepreddy@gmail.com>
2022-06-10 13:30:40 +0530

    Filtered Events Logic Added

--- pages/events/[...slug].jsx
+++ pages/events/[...slug].jsx
@@ -1,4 +1,29 @@
+import { useRouter } from "next/router";
+
 function FilteredEventsPage() {
+  const router = useRouter();
+  const filteredData = router.query.slug; // returns an array
+
+  if (!filteredData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filteredData[0];
+  const filteredMonth = filteredData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid Input.</p>;
+  }
   return <div>Filtered Events</div>;
 }
 
--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,12 +1,20 @@
+import { useRouter } from "next/router";
+
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/EventsSearch";
 
 function AllEventsPage() {
+  const router = useRouter();
   const allEvents = getAllEvents();
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
+
   return (
     <div>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={allEvents} />
     </div>
   );
